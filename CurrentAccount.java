295cf02b82ac14fc2da18166a8c586674da370a4
Jorffee <mybusinessf16@gmail.com>
2026-06-26 19:08:27 +0630

    Add applyInterest method to CurrentAccount

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -6,4 +6,11 @@ public class CurrentAccount extends Account{
     protected CurrentAccount(int accountNumber,String owner_name,double balance) {
         super(accountNumber,owner_name,balance);
     }
+
+    @Override
+    public void applyInterest(){
+
+        System.out.println("Current Account does not support interest.");
+
+    }
 }
